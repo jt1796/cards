4ff42fc5f4a4df8b24906e14066f13c738bd3de9
John Tompkins <tompkins.jt@gmail.com>
2016-03-23 19:16:38 -0400

    show card titles, add function to post card

--- public/javascript/views/cards.js
+++ public/javascript/views/cards.js
@@ -4,11 +4,36 @@ var Cards = Backbone.View.extend({
     this.listenTo(this.model, 'sync', this.render);
   },
   events: {
-
+    'click #cardCreateButton': 'createCard'
   },
   render: function() {
-    console.log(this.model.models);
-    $(this.el).html(cardsTemplate({model: this.model.models}));
+    $(this.el).html(cardsTemplate(this.processDeck()));
     return this;
+  },
+  processDeck: function() {
+    var deck = {
+      ready: [],
+      progress: [],
+      done: []
+    };
+    var data = this.model.models;
+    for(var i = 0; i < data.length; i++) {
+      var card = data[i].attributes;
+      if(card.stack == 'ready') {
+        deck.ready.push(card);
+      }
+      if(card.stack == 'progress') {
+        deck.progress.push(card);
+      }
+      if(true) {
+
+      }
+    }
+    return deck;
+  },
+  createCard: function(e) {
+    e.preventDefault();
+    alert('creating a card');
+    //in this.model, add in a card to ready and sync
   }
 })
--- public/template/cards.handlebars.js
+++ public/template/cards.handlebars.js
@@ -16,7 +16,6 @@ var cardsTemplate = Handlebars.compile(`
     </div>
   </div>
 
-
   <div class="row">
     <div class="well col-md-2">
       <form>
@@ -24,21 +23,31 @@ var cardsTemplate = Handlebars.compile(`
           <label for="cardTitle">Title</label>
           <input class="form-control" id="cardTitle">
         </div>
-        <button class="btn btn-default" id="cardButton">Create</button>
+        <div class="form-group">
+          <label for="cardBody">Body</label>
+          <input class="form-control" id="cardBody">
+        </div>
+        <div class="form-group">
+          <label for="cardAcceptance">Acceptance</label>
+          <input class="form-control" id="cardAcceptance">
+        </div>
+        <button class="btn btn-default" id="cardCreateButton">Create</button>
       </form>
     </div>
     <div class="well col-md-3">
-      {{#each model}}
-        {{#if this.attributes.stack == 'ready'}}
-          <div>
-          {{this.attributes.title}}
-          </div><br/>
-        {{/if}}
+      {{#each ready}}
+        {{this.title}}
       {{/each}}
     </div>
     <div class="well col-md-4">
+      {{#each progress}}
+        {{this.title}}
+      {{/each}}
     </div>
     <div class="well col-md-3">
+      {{#each done}}
+        {{this}}
+      {{/each}}
     </div>
   </div>
 
